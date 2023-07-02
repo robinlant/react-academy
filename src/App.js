3255037601514e04a14906fffc5f7a270a1d2a8a
@robinlant <mtarasovets08@gmail.com>
2023-07-03 00:34:54 +0200

    implementing MyModal _not_stable

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/app.css';
 import PostList from "./Components/PostList";
 import PostForm from "./Components/PostForm";
 import PostFilter from "./Components/PostFilter";
+import MyModal from "./Components/UI/modal/MyModal";
 
 function App() {
     const [posts,setPosts] = useState([
@@ -11,6 +12,7 @@ function App() {
         {id:3,title:'zz',body:'zzDescription'}
     ]);
     const [filter,setFilter] = useState({sort:'title',query:''})
+
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
             return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
@@ -31,11 +33,12 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter((p)=>p.id !== post.id))
     }
-
+    //<hr style={{margin:'15px 0',backgroundColor:'teal',border:'none',height:'1px'}}/>
     return (
         <div className="App">
-            <PostForm  createPost={createPost}/>
-            <hr style={{margin:'15px 0',backgroundColor:'teal',border:'none',height:'1px'}}/>
+            <MyModal visible={true}>
+                <PostForm  createPost={createPost}/>
+            </MyModal>
             <PostFilter filter={filter} setFilter={setFilter}/>
             <PostList posts={searchedAndSortedPosts} title={'List'} removePost={removePost}/>
         </div>
